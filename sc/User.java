5807c8412f473e407408dc253dfaabbef2477842
[sriknath] <[text.pns@gmail.com]>
2023-12-07 17:40:03 +0530

    Rental Camera

---
+++ sc/User.java
@@ -0,0 +1,27 @@
+package phase1Project;
+
+class User {
+private String username;
+private String password;
+private double walletBalance;
+public User(String username, String password, double walletBalance) {
+this.username = username;
+this.password = password;
+this.walletBalance = walletBalance;
+}
+public String getUsername() {
+return username;
+}
+public String getPassword() {
+return password;
+}
+public double getWalletBalance() {
+return walletBalance;
+}
+public void depositToWallet(double amount) {
+walletBalance += amount;
+}
+public void deductFromWallet(double amount) {
+walletBalance -= amount;
+}
+}
